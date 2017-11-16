9295fea4f6101ef7d4cbe46d7c4695843633f6c2
Jan-Olav Eide <janolaveide@gmail.com>
2017-11-16 15:33:52 +0100

    test logging

--- src/main/java/no/nav/nais/example/NaisExampleApplication.java
+++ src/main/java/no/nav/nais/example/NaisExampleApplication.java
@@ -1,12 +1,16 @@
 package no.nav.nais.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class  NaisExampleApplication {
-
+	
+	private static final Logger LOG = LoggerFactory.getLogger(NaisExampleApplication.class);
   public static void main(String[] args) {
+	  LOG.info("This thing is starting up");
     SpringApplication.run(NaisExampleApplication.class, args);
   }
 }
